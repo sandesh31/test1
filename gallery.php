9cc40ab542506b8d1efea4a78b1a837e0730aea8
unpluggedindia01 <sandesh@unpluggedindia.com>
2018-08-27 16:22:41 +0530

    gallery

---
+++ gallery.php
@@ -0,0 +1,50 @@
+<?php $page = "gallery"; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Srujan</title>
+
+</head>
+<body>
+<?php include('_header.php'); ?> 
+
+<!--banner-->
+<div class="sec-8">
+ <div class="container container1">
+    <div class="row  ">
+      <div class="col-md-12">
+         <p class="text9">Coming soon <br> <img src="images/line3.jpg" class=""></p>
+      </div>    
+    </div>
+ </div>
+</div>
+
+
+
+
+<div class="footer">
+  <div class="container container2">
+    <div class="row">
+       <div class=" col-md-6 left-cntnt">
+          <span class="mrg-btm6">© 2018. Srujan Sports Pvt Ltd. All Rights Reserved</span>
+          <span><a href="">Privacy Policy</a>  |  <a href="terms-and-conditions.php">Terms and Conditions</a></span>
+       </div>
+       <div class="col-md-6">
+         <ul class="ftr-soc-med ">
+           <li class="nav-item soc-item ">
+               <a href="https://www.facebook.com/SrujanAquaticCenter/" target="_blank" class="nav-link sqr fb"><i class="fab fa-facebook-f"></i></a>
+               <a href="" target="_blank" class="nav-link sqr gp"><i class="fab fa-youtube"></i></a>                                
+               <a href="" target="_blank" class="nav-link  sqr twtr"><i class="fab fa-instagram"></i></a>
+          </li> 
+       </div>
+    </div>
+  </div>
+
+</div>
+
+
+
+</body>
+</html>

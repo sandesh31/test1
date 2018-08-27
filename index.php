5fa492c1cca358eb8a25fd983ee2b976c3954a63
unpluggedindia01 <42673969+unpluggedindia01@users.noreply.github.com>
2018-08-27 11:30:18 +0530

    index

---
+++ index.php
@@ -0,0 +1,175 @@
+<?php $page = ""; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Srujan</title>
+
+</head>
+<body>
+<?php include('_header.php'); ?> 
+
+
+
+<div id="carousel-home" class="carousel carousel-fade" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-home" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-home" data-slide-to="1"></li>
+            <li data-target="#carousel-home" data-slide-to="2"></li>
+            <li data-target="#carousel-home" data-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner  ">
+            <div class="carousel-item active banner-1 d-flex align-items-center"> 
+               <div class="w-100 ">
+                <div class="container container1 ">
+                  <div class="row">
+                    <div class="col-md-12">
+                       <div class="banner-home-text  ml-auto">
+                         <h1 class="text1 mrg-btm">Welcome to Srujan Aquatic Center </h1>
+                         <!--<a href="about-us.php" class="btn1">Know more</a>-->
+                       </div>
+                    </div>    
+                  </div>
+                 </div> 
+               </div>
+            </div>                  
+            <div class="carousel-item  banner-2 d-flex align-items-center"> 
+               <div class="w-100 ">
+                <div class="container container1">
+                  <div class="row">
+                    <div class="col-md-12">
+                       <div class="banner-home-text ml-auto banner-home-text-b">
+                         <h1 class="text1 mrg-btm">State of the Art, Indoor warm pools </h1>
+                          <!--<a href="about-us.php" class="btn1">Know more</a>-->
+                       </div>
+                    </div>    
+                  </div>
+                 </div> 
+               </div>
+            </div>   
+            <div class="carousel-item  banner-3 d-flex align-items-center"> 
+               <div class="w-100 ">
+                <div class="container container1">
+                  <div class="row">
+                    <div class="col-md-12">
+                       <div class="banner-home-text banner-home-text-a   ml-auto">
+                         <h1 class="text1 mrg-btm">Exercise your body, develop stamina & endurance </h1>
+                          <!--<a href="about-us.php" class="btn1">Know more</a>-->
+                       </div>
+                    </div>    
+                  </div>
+                 </div> 
+               </div>
+            </div>   
+            <div class="carousel-item  banner-4 d-flex align-items-center"> 
+               <div class="w-100 ">
+                <div class="container container1">
+                  <div class="row">
+                    <div class="col-md-12">
+                       <div class="banner-home-text  ml-auto">
+                         <h1 class="text1 mrg-btm">Refresh your mind </h1>
+                          <!--<a href="about-us.php" class="btn1">Know more</a>-->
+                       </div>
+                    </div>    
+                  </div>
+                 </div> 
+               </div>
+            </div>                 
+        </div>
+</div>
+
+
+<!--sec-11-->
+<div class="sec-11 txt-cntr">
+   <div class="container">
+     <div class="row justify-content-between">
+      <div class="col-md-12 ">
+         <h1 class="text8  mrg-btm">Benefits of swimming</h1>
+      </div>
+        <div class="col-xl-3 col-lg-4 col-md-4">
+           <img src="images/benefits-1.png" class="img-fluid mrg-btm3"/>
+           <p class="text3a mrg-btm7">Excellent Cardio Vascular exercise, Improves Lungs intake and capacity.</p>
+        </div>
+        <div class="col-xl-3 col-lg-4 col-md-4">
+           <img src="images/benefits-2.png" class="img-fluid mrg-btm3"/>
+           <p class="text3a mrg-btm7">Reduces Stress and hypertension, Overall Helps in Controlling and reducing weight</p>
+        </div>
+        <div class="col-xl-3 col-lg-4 col-md-4">
+           <img src="images/benefits-3.png" class="img-fluid mrg-btm3"/>
+           <p class="text3a">Helps toning of muscles and improves immunity.</p>
+        </div>
+     </div>
+   </div>
+</div>
+
+
+<!--sec-2-->
+<div class="sec-2">
+   <div class="container container1">
+      <div class="row">
+         <div class="col-lg-5 col-md-6 bg-blue">
+           <div class="sec-2a">
+             <p class="text4">Schedule <br/><img src="images/line2.png" class="img1"/></p>
+              <div class="btch mrng-btn">                 
+                    <span>Members / Coaching</span><span class="rgt-img ml-auto"><img src="images/icon-right-chev.png" class="img2"/></span>                 
+               </div>
+               <div class="btch aftr-btn">                 
+                    <span>Exclusive Women's Batch </span><span class="rgt-img ml-auto"><img src="images/icon-right-chev.png" class="img2"/></span>                 
+               </div>
+               <div class="btch eve-btn">                 
+                    <span>Common Batch</span><span class="rgt-img ml-auto"><img src="images/icon-right-chev.png" class="img2"/></span>                 
+               </div>                          
+           </div>
+         </div>
+         <div class="col-lg-7 col-md-6 txt-cntr"> 
+           <div class="sec-2b btch-wrp" id="mrng-btc">
+            <p class="text4 mrg-btm1">Members / Coaching </p>
+            <!--<p class="text3">Timings - 5 Hrs / 5 Days / 20 days Classes</p>-->
+            <p class="hr1"></p>
+            <div class="row justify-content-center">
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">05 AM - 06 AM</p><!--<p class="text6">Common Batch M/F</p>--></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">06 AM - 07 AM</p><!--<p class="text6">Batch</p>--></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">07 AM - 08 AM</p><!--<p class="text6">Batch</p>--></div></div> 
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">08 AM- 09 AM</p><!--<p class="text6">Children’s Batch</p>--></div></div>                 
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">05 PM - 06 PM</p></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">06 PM - 07 PM</p></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">07 PM - 08 PM</p></div></div> 
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">08 PM - 09 PM</p></div></div>                        
+            </div>
+            </div>
+            <div class="sec-2b btch-wrp" id="aftr-btc">
+            <p class="text4 mrg-btm1">Exclusive Women's Batch </p>           
+            <p class="hr1"></p>
+            <div class="row justify-content-center">                
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">09 AM- 10 AM</p></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">10 AM- 11  AM</p></div></div> 
+                 <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">11 AM- 12  Noon</p></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">12 Noon- 01 PM </p></div></div>                          
+            </div>
+            </div>
+            <div class="sec-2b btch-wrp" id="eve-btc">
+            <p class="text4 mrg-btm1">Common Batch</p>           
+            <p class="hr1"></p>
+            <div class="row justify-content-center">
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">01 PM - 02 PM </p></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">02 PM - 03 PM </p></div></div>
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">03 PM - 04 PM </p></div></div>  
+                <div class="col-md-4 col-6"><div class="sec-2b-wrap"><p class="text5 mrg-btm2">04 PM - 05 PM </p></div></div>                                         
+            </div>
+            </div>           
+         </div>
+      </div>
+   </div>
+</div>   
+
+
+<!-- Map -->
+<div class="map">
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d62215.59767472316!2d77.6028143!3d12.9414379!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xad11ee6d49f0523d!2sAdena+Sports!5e0!3m2!1sen!2sin!4v1525939242385"  frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+
+<?php include('_footer.php'); ?> 
+</body>
+</html>
